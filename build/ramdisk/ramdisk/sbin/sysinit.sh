ebad4651ecd0d6ffe266e1c06719e75b06eca6d6
djb77 <dwayne.bakewell@gmail.com>
2017-09-18 12:38:55 +1000

    ramdisk: Add init.d Support
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- build/ramdisk/ramdisk/sbin/sysinit.sh
+++ build/ramdisk/ramdisk/sbin/sysinit.sh
@@ -16,5 +16,17 @@
 mount -o remount,rw /;
 mount -o rw,remount /system
 
+# init.d support
+if [ ! -e /system/etc/init.d ]; then
+	mkdir /system/etc/init.d
+	chown -R root.root /system/etc/init.d
+	chmod -R 755 /system/etc/init.d
+fi
+
+# start init.d
+for FILE in /system/etc/init.d/*; do
+	sh $FILE >/dev/null
+done;
+
 # Advanced Mods
 
