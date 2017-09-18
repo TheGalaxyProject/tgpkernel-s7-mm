83844466b3f30125854b50dbc1b5309afd483269
djb77 <dwayne.bakewell@gmail.com>
2017-09-18 12:32:01 +1000

    ramdisk: Set up init.services.rc
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

---
+++ build/ramdisk/ramdisk/sbin/kernelinit.sh
@@ -0,0 +1,37 @@
+#!/system/bin/sh
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Busybox
+if [ -e /su/xbin/busybox ]; then
+	BB=/su/xbin/busybox;
+else if [ -e /sbin/busybox ]; then
+	BB=/sbin/busybox;
+else
+	BB=/system/xbin/busybox;
+fi;
+fi;
+
+# Mount
+$BB mount -t rootfs -o remount,rw rootfs;
+$BB mount -o remount,rw /system;
+$BB mount -o remount,rw /data;
+$BB mount -o remount,rw /;
+
+# Unmount
+$BB mount -t rootfs -o remount,rw rootfs;
+$BB mount -o remount,ro /system;
+$BB mount -o remount,rw /data;
+$BB mount -o remount,ro /;
+
